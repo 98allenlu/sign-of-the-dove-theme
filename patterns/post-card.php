1c658fa3e3f36177fdef49594c9691dea97b4429
98allenlu <85421725+98allenlu@users.noreply.github.com>
2026-06-11 12:38:43 -0400

    Update post-card.php

---
+++ patterns/post-card.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Post Card
+ * Slug: sign-of-the-dove/post-card
+ * Categories: query
+ */
+?>
+<!-- wp:group {"className":"custom-post-card","layout":{"type":"constrained"}} -->
+<div class="wp-block-group custom-post-card">
+    <!-- wp:post-featured-image /-->
+    <!-- wp:post-title {"isLink":true,"level":3} /-->
+    <!-- wp:post-excerpt {"moreText":"Read More"} /-->
+</div>
+<!-- /wp:group -->
